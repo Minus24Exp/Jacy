0c60a47f419419035a57b8ee4b1003cb77aceaaa
hazer_hazer <gordeev.john@gmail.com>
2020-07-27 17:49:23 +0300

    I forgot about b00l hash)))

--- src/object/Bool.cpp
+++ src/object/Bool.cpp
@@ -1,7 +1,10 @@
 #include "object/Bool.h"
 #include "object/String.h"
+#include "object/Int.h"
 
 Bool::Bool(bool b) : Object(ObjectType::Bool, cBool), value(b)
 {
-    // TODO: hash function
+    define_builtin("hash", make_nf(nullptr, "hash", {}, [this](NFArgs && args){
+        return std::make_shared<Int>(static_cast<yo_int>(std::hash<bool>{}(value)));
+    }));
 }
